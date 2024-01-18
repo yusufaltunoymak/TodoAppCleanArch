e5271847492e929abfe799ebca437546384a9fff
altunoymak <yusuf_altunoymak@hotmail.com>
2024-01-18 17:10:52 +0300

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,77 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    kotlin("kapt")
+
+    alias(libs.plugins.com.android.application)
+    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.com.google.devtools.kts)
+    alias(libs.plugins.com.google.dagger.hilt.android)
+    alias(libs.plugins.navigation.safeargs)
+}
+
+android {
+    namespace = "com.example.todoappcleanarch"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.todoappcleanarch"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.constraintlayout)
+    implementation(libs.roomdb)
+    implementation(libs.roomKtx)
+    implementation(libs.hilt)
+    implementation(libs.coroutines)
+    implementation(libs.navigationFragment)
+    implementation(libs.navigationUi)
+    implementation(libs.viewmodel)
+    implementation(libs.livedata)
+    implementation(libs.lifecycle)
+    implementation(libs.savedsatateViewmodel)
+
+    ksp(libs.roomdbCompiler)
+    ksp(libs.hiltCompiler)
+
+    kapt(libs.lifecycleCompiler)
+
+    testImplementation(libs.junit)
+
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
+
+kapt {
+    correctErrorTypes = true
+}
